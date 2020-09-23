220240b6faed5b295471e0286e2e769af0c03838
Adam Payne <ajpayne238@gmail.com>
2020-09-23 15:05:30 -0400

    added mutations for ProductPurchase forms

--- product-app/src/components/ProductPurchase.js
+++ product-app/src/components/ProductPurchase.js
@@ -6,6 +6,7 @@ const ProductPurchase = () => {
     return(
         <div>
             <h1>ProductPurchase</h1>
+            
         </div>
     )
 }
--- product-app/src/resolvers/Resolvers.js
+++ product-app/src/resolvers/Resolvers.js
@@ -1,6 +1,7 @@
 import {gql} from 'apollo-boost'
 
 
+// QUERY PRODUCT LIST
 export const PRODUCTS = gql`
     query {
         products {
@@ -14,6 +15,8 @@ export const PRODUCTS = gql`
     }
 `
 
+
+// QUERY PRODUCT DETAILS
 export const PRODUCT_DETAILS = gql`
     query product($id: String!){
         product(id: $id){
@@ -27,6 +30,119 @@ export const PRODUCT_DETAILS = gql`
             callback
             category
             inventory_on_hand
+        }
+    }
+`
+
+
+// CREATE ORDER
+export const CREATE_ORDER = gql`
+    mutation CREATE_ORDER(
+        $customer_name: String!
+        $customer_email: String!
+    ){
+        createOrder(
+            customer_name: $customer_name
+            customer_email: $customer_email
+        ){
+            id
+            customer_name
+            customer_email
+        }
+    }
+`
+
+
+// CREATE PHONE
+export const CREATE_PHONE = gql`
+    mutation CREATE_PHONE(
+        $number: Int!
+        $type: String!
+        $contact: Boolean!
+        $order_id: String!
+    ){
+        createPhone(
+            number: $number
+            type: $type
+            contact: $contact
+            order_id: $order_id
+        ){
+            number
+            type
+            contact
+            order_id
+        }
+    }
+`
+
+
+// CREATE SHIPPING
+export const CREATE_SHIPPING = gql`
+    mutation CREATE_SHIPPING(
+        $street: String!
+        $city: String!
+        $state: String!
+        $zipcode: Int!
+        $order_id: String!
+    ){
+        createShipping(
+            street: $street
+            city: $city
+            state: $state
+            zipcode: $zipcode
+            order_id: $order_id
+        ){
+            street
+            city
+            state
+            zipcode
+            order_id
+        }
+    }
+`
+
+
+// CREATE BILLING
+export const CREATE_BILLING = gql`
+    mutation CREATE_BILLING(
+        $street: String!
+        $city: String!
+        $state: String!
+        $zipcode: Int!
+        $order_id: String!
+    ){
+        createBilling(
+            street: $street
+            city: $city
+            state: $state
+            zipcode: $zipcode
+            order_id: $order_id
+        ){
+            street
+            city
+            state
+            zipcode
+            order_id
+        }
+    }
+`
+
+
+// CREATE PURCHASE
+export const CREATE_PURCHASE = gql`
+    mutation CREATE_PURCHASE(
+        $code: Int!
+        $quantity: Int!
+        $order_id: String!
+    ){
+        createPurchase(
+            code: $code
+            quantity: $quantity
+            order_id: $order_id
+        ){
+            code
+            quantity
+            order_id
         }
     }
 `
