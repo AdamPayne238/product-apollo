4a66a25f97fadf64ad17ca9640771ab7e24f1a69
Adam Payne <ajpayne238@gmail.com>
2020-11-06 21:16:28 -0500

    adding content layout to homepage

--- product-app/src/components/Home/Home.js
+++ product-app/src/components/Home/Home.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import banner from '../../global/images/banner.jpeg'
+import imageOne from '../../global/images/abstract-fruit-1.jpg'
+import imageTwo from '../../global/images/abstract-fruit-2.jpg'
+import imageThree from '../../global/images/abstract-fruit-3.jpg'
+import imageFour from '../../global/images/abstract-fruit-4.jpg'
+
 
 
 const Home = () => {
@@ -11,6 +16,42 @@ const Home = () => {
             <div className="home-banner">
                 <img src={banner} />
             </div>
+
+            <div className="section-one">
+                <div className="section-one-text">
+                    <p>section one text</p>
+                </div>
+                <div className="section-one-img">
+                    <img src={imageOne} />
+                </div>
+            </div>
+
+            {/* <div className="section-two">
+                <div className="section-two-text">
+                    <p>section two text</p>
+                </div>
+                <div className="section-two-img">
+                    <img src={imageTwo} />
+                </div>
+            </div>
+
+            <div className="section-three">
+                <div className="section-three-text">
+                    <p>section three text</p>
+                </div>
+                <div className="section-three-img">
+                    <img src={imageThree} />
+                </div>
+            </div>
+
+            <div className="section-four">
+                <div className="section-four-text">
+                    <p>section four text</p>
+                </div>
+                <div className="section-four-img">
+                    <img src={imageFour} />
+                </div>
+            </div> */}
 
         </div>
     )
